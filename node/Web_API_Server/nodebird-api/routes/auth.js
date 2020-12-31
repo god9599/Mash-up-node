696cac3c9598bfbbf9188b52f83f57d1acac4206
god9599 <59276134+god9599@users.noreply.github.com>
2021-01-01 00:43:06 +0900

    use JWT

--- node/Web_API_Server/nodebird-api/routes/auth.js
+++ node/Web_API_Server/nodebird-api/routes/auth.js
@@ -9,14 +9,14 @@ const router = express.Router();
 router.post('/join', isNotLoggedIn, async(req, res, next)=>{
     const {email, nick, password} = req.body;
     try{
-        const exUser = await User.findOne({where:{email}});
+        const exUser = await User.find({where:{email}});
         if(exUser){
             return res.redirect('/join?error=exist');
         }
         const hash = await bcrypt.hash(password, 12);
         await User.create({
-            email,
-            nick,
+            email : email,
+            nick : nick,
             password : hash,
         });
         return res.redirect('/');
@@ -25,6 +25,7 @@ router.post('/join', isNotLoggedIn, async(req, res, next)=>{
         return next(error);
     }
 });
+
 
 router.post('/login', isNotLoggedIn, (req, res, next)=>{
     passport.authenticate('local', (authError, user, info)=>{
